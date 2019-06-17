f91e0d092e17ed8831b8067c13d7c1a94b8b2954
arodriguez <51332629+arodriguezSE@users.noreply.github.com>
2019-06-17 18:00:17 +0200

    Added username and create chat features.Added list chats with ajax

--- application.py
+++ application.py
@@ -1,16 +1,33 @@
 import os
 
-from flask import Flask
+from flask import Flask, render_template,request,jsonify
 from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 socketio = SocketIO(app)
 
+chats=[]
 
 @app.route("/")
 def index():
-    return "Project 2: TODO"
+    #return "Project 2: TODO"
+    return render_template("index.html",chats=chats)
+@app.route("/newchat", methods=["POST"])
+def newchat():
+
+    newChatName=request.form.get("chatname")
+
+    if newChatName in chats:
+        return jsonify({"success": False})
+
+    else:
+        chats.append(newChatName)
+        return jsonify({"success": True })
+
+
+
+
 
 if __name__ == '__main__':
     socketio.run(app, debug=True)
