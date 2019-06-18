772d6de7c2f4ea93de48c008a3a654647ef23c26
arodriguez <51332629+arodriguezSE@users.noreply.github.com>
2019-06-18 19:21:19 +0200

    Added messaging with websockets feature

--- application.py
+++ application.py
@@ -2,36 +2,64 @@ import os
 
 from flask import Flask, render_template,request,jsonify
 from flask_socketio import SocketIO, emit
+from collections import deque
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 socketio = SocketIO(app)
 
+class Message:
+    def __init__(self, text, user,date,time):
+        self.text = text
+        self.user = user
+        self.date = date
+        self.time = time
+
 chats=[]
+#this will be a list of deques
+messages=[]
 
 @app.route("/")
 def index():
-    #return "Project 2: TODO"
+
     return render_template("index.html",chats=chats)
 
 @socketio.on("new chat petition")
 def newchat(data):
-    print("petition")
+    #print("petition")
     newChatName = data["newchatname"]
     #se puede hacer sin broadcast?
     if newChatName in chats:
         emit("new chat",{"success": False}, broadcast=True)
-        print("Taken "+ newChatName)
+        #print("Taken "+ newChatName)
     else:
         chats.append(newChatName)
         newIndex = len(chats)-1
-        print("New index number: "+str(newIndex))
+        #create deque of messages for new chat
+        messages.append(deque(maxlen=100))
+        #print("New index number: "+str(newIndex))
         emit("new chat", {"success": True, "newChatName" : newChatName, "newIndex": newIndex}, broadcast=True)
-        print("Available "+ newChatName)
+        #print("Available "+ newChatName)
+
+@socketio.on("send message")
+def newmessage(data):
+    print("message in server")
+    newmessage=data["newmessage"]
+    username=data["username"]
+    chatnumber=data["chatnumber"]
+    date=data["date"]
+    time=data["time"]
+    m=Message(newmessage,username,date,time)
+    #qMsg=messages[chatnumber]
+    messages[chatnumber].append(m)
+    emit("new message", {"newmessage": newmessage, "username": username, "chatnumber": chatnumber,  "date":date,"time": time}, broadcast=True)
+    print("message broadcasted")
+
 
 @app.route("/<int:chatnumber>")
 def chatroom(chatnumber):
-    return (render_template("chatview.html",chatnumber=chatnumber))
+    qMsg=messages[chatnumber]
+    return (render_template("chatview.html",chatnumber=chatnumber,messages=qMsg))
 
 
 
