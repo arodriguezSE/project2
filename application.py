1e92f5482b663d5aadf1e33368a7e0fce569aa4c
arodriguez <51332629+arodriguezSE@users.noreply.github.com>
2019-06-18 11:14:07 +0200

    Changed new chat feature to use websockets

--- application.py
+++ application.py
@@ -13,17 +13,42 @@ chats=[]
 def index():
     #return "Project 2: TODO"
     return render_template("index.html",chats=chats)
-@app.route("/newchat", methods=["POST"])
-def newchat():
+# @app.route("/newchat", methods=["POST"])
+# def newchat():
+#
+#     newChatName=request.form.get("chatname")
+#     print("got request to create" + newChatName)
+#
+#     if newChatName in chats:
+#         #return jsonify({"success": False})
+#         emit("new",{"success": False}, broadcast=True)
+#         print("used name")
+#
+#     else:
+#         chats.append(newChatName)
+#         print("not used name")
+#         return jsonify({"success": True })
+#         print("trying")
+#         emit("new", {"success": True, "newChatName" : newChatName}, broadcast=True)
+#         #emit("chat added", {"newChatName" : newChatName}, broadcast=True)
+#         print("event emited")
 
-    newChatName=request.form.get("chatname")
-
+@socketio.on("new chat petition")
+def newchat(data):
+    print("petition")
+    newChatName = data["newchatname"]
+    #se puede hacer sin broadcast?
     if newChatName in chats:
-        return jsonify({"success": False})
-
+        emit("new chat",{"success": False}, broadcast=True)
+        print("Taken "+ newChatName)
     else:
         chats.append(newChatName)
-        return jsonify({"success": True })
+        emit("new chat", {"success": True, "newChatName" : newChatName}, broadcast=True)
+        print("Available "+ newChatName)
+    #print(newchatname)
+    #emit("announce vote", {"selection": selection}, broadcast=True)
+
+
 
 
 
